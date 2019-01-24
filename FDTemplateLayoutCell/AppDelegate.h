f4cd0a7320082166e3cfae611c772bdae6d42532
liliguang <445948630@qq.com>
2019-01-24 10:03:19 +0800

    创建关于FDTemplateLayoutCell
    创建研究FDTemplateLayoutCell 的Demo

---
+++ FDTemplateLayoutCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FDTemplateLayoutCell
+//
+//  Created by liliguang on 2019/1/24.
+//  Copyright © 2019 llg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FDTemplateLayoutCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDTemplateLayoutCell
+//
+//  Created by liliguang on 2019/1/24.
+//  Copyright © 2019 llg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
